921c2bcdaba25fc73a87c0c67a8687699eda0e0b
iSparsh <mishra.sparsh18@gmail.com>
2021-01-24 17:01:17 +0530

    added patient registry
    added the startings of the patient_registry system into the repository

---
+++ patient_registry.py
@@ -0,0 +1,12 @@
+"""
+Creator: Sparsh Mishra
+Date: 24/01/21
+Description:
+The first developing application for a simple patient
+registry system. Records all the necessary info.
+
+A GUI Applicatioan that helps to push data to a future SQL
+server or lically in a spreadsheet or a CSV.
+
+"""
+import tkinter as tk
